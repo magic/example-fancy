a460f2fa00b273fe012147e5190534e0d23f1664
Jascha <jascha@jaeh.at>
2019-06-08 17:12:35 +0200

    modules/Route add content, css

--- src/assets/Route.mjs
+++ src/assets/Route.mjs
@@ -2,8 +2,16 @@ export const View = ({ page, state }) => [
   Header(state),
   div({ class: 'Page' }, page),
   Footer(state,
-    div({ class: 'FooterMenuWrapper' }, [
-      div({ class: 'FooterMenu'}, [
+    div({ class: 'Menus'}, [
+      div({ class: 'Child Info' }, [
+        h2('@magic'),
+        p('isomorphic web app generator.'),
+        p('faas based graphql api.'),
+        p('static pages or ssr.'),
+        p('deduplicates css selectors.'),
+        p('works without javascript.'),
+      ]),
+      div({ class: 'Child'}, [
         h2('Magic github organizations'),
         Menu({
           items: [
@@ -12,9 +20,10 @@ export const View = ({ page, state }) => [
             { text: '@magic-themes', to: 'https://magic-themes.github.io/'},
             { text: '@magic-libraries', to: 'https://magic-libraries.github.io/'},
           ],
+          state,
         }),
       ]),
-      div({ class: 'FooterMenu' }, [
+      div({ class: 'Child' }, [
         h2('Magic npm organizations'),
         Menu({
           items: [
@@ -23,32 +32,62 @@ export const View = ({ page, state }) => [
             { text: '@magic-themes', to: 'https://www.npmjs.com/org/magic-themes' },
             { text: '@magic-libraries', to: 'https://www.npmjs.com/org/magic-libraries' },
           ],
+          state,
         }),
       ]),
     ]),
   ),
 ]
 
-export const style = {
+export const style = (vars = {}) =>  ({
   '.Route': {},
-  '.FooterMenuWrapper': {
-    margin: '0 auto',
-    width: '80%',
-    display: 'inline-block',
-  },
-  '.FooterMenu': {
-    float: 'left',
-    width: '45%',
-    margin: '0 2.5%',
+  '.Footer': {
+    backgroundColor: vars.colors.gray[900],
+    color: vars.colors.gray[400],
 
-    '.Menu': {
-      float: 'none',
-    },
+    a: {
+      color: vars.colors.gray[100],
+      textDecoration: 'underline',
 
-    ul: {
-      li: {
-        float: 'none',
+      '&:hover': {
+        color: vars.colors.gray[300],
       },
     },
+
+    '.Container': {
+      margin: '0 auto',
+      width: '80%',
+      display: 'inline-block',
+    },
+
+    '.Info': {
+      fontSize: '.9em',
+    },
+
+    '.Menus': {
+      display: 'inline-block',
+    },
+    '.Child': {
+      '.Menu': {
+        float: 'none',
+      },
+
+      ul: {
+        li: {
+          float: 'none',
+        },
+      },
+    },
+    '.Credits': {
+      margin: '2em 0 1em',
+    },
   },
-}
+
+  '@media screen and (min-width: 800px)': {
+    '.Child': {
+      float: 'left',
+      width: '30%',
+      margin: '0 1.5%',
+    },
+  },
+})
